4c4faafae9fa377e2178987c77ec781e8bece857
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-04 23:44:39 +0500

    1

--- file01.py
+++ file01.py
@@ -6,5 +6,10 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    data=open('txt_file\data01.txt',mode='r+')
+    l=[]
+    l.append(data.readline())
+    return l
 # Read data from file
+data=str()
+print(main(data))

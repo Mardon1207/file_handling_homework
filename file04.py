0f2779d18836cb9684b3b8c615a0238623aedb1b
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-05 09:32:10 +0500

    4

--- file04.py
+++ file04.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f=open('txt_file/data04.txt',mode='r')
+    txt=f.read()
+    lst=[]
+    for i in txt:
+        lst.append(i)
+    return lst
 # Read data from file
+print(main('txt_file/data04.txt'))

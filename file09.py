0a46c5f0f0ac987386b94583f447cd68a1e40369
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-05 10:19:11 +0500

    9

--- file09.py
+++ file09.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    f=open('txt_file/data08.txt',mode='r')
+    txt=f.read()
+    s=10
+    lst=[]
+    for i in txt:
+        if i.isdigit():
+            if int(i)<s:
+                s=int(i)
+    return s
 # Read data from file
+print(main('txt_file/data08.txt'))

99c38fecbcec86352e07f8956ae3cadc26e89f24
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-05 10:35:43 +0500

    6

--- file06.py
+++ file06.py
@@ -6,16 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    f=open('txt_file/data03.txt',mode='r')
+    f=open('txt_file\data06.txt', mode='r')
     txt=f.read()
-    s=txt.split('/n')
-    m=str()
-    l=[]
-    k=0
+    lst=[]
+    s=txt.split('\n')
     for i in s:
-        k=len(i)
-        l=l.append(k)
-    return l
-# Read data from file
-data=str()
-print(main('txt_file\data06.txt'))
+        lst.append(len(i))
+    return lst
+print(main('txt_file/data06.txt'))
